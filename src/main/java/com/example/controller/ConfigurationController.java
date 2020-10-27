eedf63a76f9fa8942b30e691bde69541180ff58f
Arunim Roy <arunim.roy@philips.com>
2020-10-27 23:33:04 +0530

    Changes made

--- src/main/java/com/example/controller/ConfigurationController.java
+++ src/main/java/com/example/controller/ConfigurationController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.controller;
 
 import com.example.dto.ClientDTO;
@@ -80,69 +79,3 @@ public class ConfigurationController {
     }
 
 }
-=======
-package com.example.controller;
-
-import com.example.dto.ClientDTO;
-import com.example.entities.Client;
-import com.example.exceptions.ClientAlreadyExistsException;
-import com.example.service.BedService;
-import com.example.service.ClientService;
-import com.example.service.DeviceService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.UUID;
-import java.util.List;
-
-@CrossOrigin(origins = "http://localhost:4200")
-@RestController
-@RequestMapping("/pms")
-public class ConfigurationController {
-	
-	final Logger logger = LogManager.getLogger(ConfigurationController.class);
-    @Autowired
-    private ClientService clientService;
-
-    @Autowired
-    private BedService bedService;
-
-    @Autowired
-    private DeviceService deviceService;
-
-    @CrossOrigin(origins = "http://localhost:4200")
-    @PostMapping("/client/config")
-    public ResponseEntity<Client> createClient(@Valid @RequestBody ClientDTO clientDTO) throws ClientAlreadyExistsException {
-        Client savedClient = clientService.saveClient(clientDTO);
-        logger.info("Saving client");
-        bedService.createBeds(savedClient.getNo_of_beds(), savedClient.getClient_id());
-        logger.info("Saving Beds");
-        deviceService.createDevices(savedClient.getNo_of_beds());
-        logger.info("Saving Devices");
-        return new ResponseEntity<Client>(savedClient, HttpStatus.CREATED);
-    }
-
-    @PutMapping("/client/{client_id}/config")
-    public ResponseEntity<Client> updateClient(@PathVariable(value = "client_id") UUID client_id, @Valid @RequestBody ClientDTO clientDTO) {
-        int no_of_beds_to_add = clientDTO.getNo_of_beds();
-        Client savedClient = clientService.updateClient(clientDTO, client_id.toString());
-        logger.info("Updating client");
-        bedService.createBeds(no_of_beds_to_add, client_id.toString());
-        logger.info("Saving Beds");
-        deviceService.createDevices(no_of_beds_to_add);
-        logger.info("Saving devices");
-        return new ResponseEntity<Client>(savedClient, HttpStatus.OK);
-    }
-
-    //api to get all clients
-    @GetMapping("/client/all")
-    public List<Client> getAllClients(){
-        return clientService.getALlClients();
-    }
-}
->>>>>>> c7f7f1d525f38a0be4d1be9bb7a35d89c079543a
--- src/main/java/com/example/service/ClientService.java
+++ src/main/java/com/example/service/ClientService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.service;
 
 import com.example.dto.ClientDTO;
@@ -54,60 +53,3 @@ public class ClientService {
         return clientRepository.findAll();
     }
 }
-=======
-package com.example.service;
-
-import com.example.dto.ClientDTO;
-import com.example.entities.Client;
-import com.example.exceptions.ClientAlreadyExistsException;
-import com.example.mapper.ClientMapper;
-import com.example.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.UUID;
-import java.util.List;
-
-@Service
-public class ClientService {
-
-    ClientRepository clientRepository;
-
-    ClientMapper clientMapper;
-
-    @Autowired
-    public ClientService(ClientRepository clientRepository,ClientMapper clientMapper){
-        this.clientRepository = clientRepository;
-        this.clientMapper = clientMapper;
-    }
-
-
-    public Client saveClient(ClientDTO clientDTO) throws ClientAlreadyExistsException {
-        Client client = clientMapper.mapClientDTOtoClientEntity(clientDTO);
-        String client_id = UUID.randomUUID().toString();
-        client.setClient_id(client_id);
-        if(clientRepository.findById(client_id).isPresent()) {
-            throw new ClientAlreadyExistsException("Client with id = "+client_id + " already exists");
-        }
-        return clientRepository.save(client);
-    }
-
-    public Client updateClient(ClientDTO clientDTO, String client_id){
-        Client client = clientMapper.mapClientDTOtoClientEntity(clientDTO);
-        client.setClient_id(client_id);
-
-        if(checkIfClientExists(client_id)) {
-            client.setNo_of_beds(clientRepository.findById(client_id).get().getNo_of_beds() + client.getNo_of_beds());
-        }
-        return clientRepository.save(client);
-    }
-
-    public boolean checkIfClientExists(String client_id){
-        return clientRepository.findById(client_id).isPresent();
-    }
-
-    public List<Client> getALlClients(){
-        return clientRepository.findAll();
-    }
-}
->>>>>>> c7f7f1d525f38a0be4d1be9bb7a35d89c079543a
